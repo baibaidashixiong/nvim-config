f53e24bf0ebf30257d5ee60bb9883c47ff222d34
zqz <z@qz.com>
2023-05-10 13:01:39 +0800

    fix buffer-switch bug, use <A-h/l> to switch buffer use <C-w> to close buffer

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,13 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
+
+local opt = {
+  noremap = true, --  mapping will not be recursive
+  silent = true,  -- the command will not display any output messages when executed
+}
+
+
 -- visual mode --
 -- move one or more line --
 -- line in visual then use shift+i/j
@@ -17,6 +24,9 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 -- plugins --
 -- nvim-tree
 keymap.set("n", "<leader>tr", ":NvimTreeToggle<CR>")
--- switch buffer
-keymap.set("n", "<C-k>", ":bnext<CR>")
-keymap.set("n", "<C-j>", ":bprevious<CR>")
+-- bufferline
+  -- switch buffer
+keymap.set("n", "<A-l>", ":bnext<CR>", opt)
+keymap.set("n", "<A-h>", ":bprevious<CR>", opt)
+  -- close buffer
+keymap.set("n", "<C-w>", ":bd<CR>", opt)
