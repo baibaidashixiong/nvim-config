a946bc4c89ee8476b7c41724ee37a406a3c2adb7
zqz <z@qz.com>
2024-05-15 11:39:08 +0800

    update

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,6 +4,17 @@ vim.g.loaded_netrwPlugin = 1
 
 -- empty setup using defaults
 require("nvim-tree").setup({
+	-- needed by project-----
+  hijack_cursor = true,
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+	},
+	filters = {
+		-- 不显示.开头的文件
+		dotfiles = true,
+		-- custom = { "" },
+	},
   git = {
     ignore = false,
   },
